d9993f15497f24386ff43ab5997b9f1c8ead3ea3
whaplzzz <whaplz222@gmail.com>
2021-01-20 21:18:09 +0300

    append search form

--- HacatonUntitledTeam/HacatonUntitledTeam/ClientApp/src/app/home/home.component.ts
+++ HacatonUntitledTeam/HacatonUntitledTeam/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  searchFormGroup!: FormGroup;  // переменная для формирование поиска данных
+  goodTitle = 'Гречка';
+
+  // констркутор
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.searchFormGroup = this.fb.group({
+      product: [this.goodTitle, [Validators.required]],
+    });
+  }
+
+  // событие при котормо будет отправка параметра
+  submit(): void {
+    console.log(this.searchFormGroup.value);
+  }
+
+  // геттер для доступа к форме
+  get product(): any { return this.searchFormGroup.controls.product; }
 }
